73102dea633e14d63f85dd8fcc336c570dff2384
pratik <pdhakal906@gmail.com>
2023-07-13 08:58:48 +0545

    changes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController')
-router.get('/', productController.getAllProducts)
+router.get('/', productController.getAllProducts);
+router.get('/product/:id', productController.getAllProducts)
 
 module.exports = router
