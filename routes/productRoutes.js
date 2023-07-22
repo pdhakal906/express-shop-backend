569530bf750de18cde027fa8caaad7e01851425b
pratik <pdhakal906@gmail.com>
2023-07-22 20:33:12 +0545

    changes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
-const fileCheck = require('../middleware/fileCheck');
+const fileCheck = require('../middleware/fileCheck.js');
 const userCheck = require('../middleware/userCheck');
 
 
